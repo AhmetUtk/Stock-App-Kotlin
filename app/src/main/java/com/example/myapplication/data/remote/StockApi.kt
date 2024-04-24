5a3dfb5cb5b8ebc62cdeeded22b67e6d4cc451aa
Ahmet_Utku <94536064+AhmetUtk@users.noreply.github.com>
2024-04-24 20:03:39 +0300

    created stock api

--- app/src/main/java/com/example/myapplication/data/remote/StockApi.kt
+++ app/src/main/java/com/example/myapplication/data/remote/StockApi.kt
@@ -1,15 +1,25 @@
 package com.example.myapplication.data.remote
 
-
-
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface StockApi {
 
+    @GET("query?function=LISTING_STATUS")
+
 
     suspend fun getListings(
 
-    )
+        @Query("apikey") apiKey:String = API_KEY
+
+    ):ResponseBody
+
+    companion object{
+        const val API_KEY = "VPLXZ9K5ML2BOW7W"
+        const val BASE_URL = "http://alphavantage.co"
+    }
 
 
 }
