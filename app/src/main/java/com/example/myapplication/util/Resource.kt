c9b37fb7deb9a9cec31ecef7b01e5a3d24db5ccc
Ahmet_Utku <94536064+AhmetUtk@users.noreply.github.com>
2024-04-24 20:02:54 +0300

    created resource

---
+++ app/src/main/java/com/example/myapplication/util/Resource.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.util
+
+sealed class Resource <T>(val data: T? = null, val massage:String? = null){
+    class Success<T>(data: T?):Resource<T>(data)
+    class Error<T>(massage: String, data: T? = null): Resource<T>(data,massage)
+    class Loading<T>(val isLoading: Boolean = true): Resource<T>(null)
+}
