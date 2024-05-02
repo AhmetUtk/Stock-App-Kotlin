57fe3a0f868f1cb74213d61ac7a3d18b000fd42d
Ahmet_Utku <94536064+AhmetUtk@users.noreply.github.com>
2024-05-02 23:56:37 +0300

    added new interface suspend function

--- app/src/main/java/com/example/myapplication/domain/repostory/StockRepository.kt
+++ app/src/main/java/com/example/myapplication/domain/repostory/StockRepository.kt
@@ -1,14 +1,24 @@
 package com.example.myapplication.domain.repostory
 
 import androidx.room.Query
+import com.example.myapplication.domain.model.CompanyInfo
 import com.example.myapplication.domain.model.CompanyListing
+import com.example.myapplication.domain.model.IntradayInfo
 import com.example.myapplication.util.Resource
+import kotlinx.coroutines.flow.Flow
 
 interface StockRepository {
 
-    suspend fun getCompanyListing(
-        fetchFromRemote : Boolean,
+    suspend fun getCompanyListings(
+        fetchFromRemote: Boolean,
         query: String
-    ): kotlinx.coroutines.flow.Flow<Resource<List<CompanyListing>>>
+    ): Flow<Resource<List<CompanyListing>>>
 
+    suspend fun getIntradayInfo(
+        symbol: String
+    ): Resource<List<IntradayInfo>>
+
+    suspend fun getCompanyInfo(
+        symbol: String
+    ): Resource<CompanyInfo>
 }
