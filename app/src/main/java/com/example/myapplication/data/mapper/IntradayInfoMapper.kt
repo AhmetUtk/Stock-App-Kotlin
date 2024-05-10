6ed8d294bc89c9c7f26aede59a60851e07327975
Ahmet_Utku <94536064+AhmetUtk@users.noreply.github.com>
2024-05-10 23:43:17 +0300

    added IntradayInfoMapper

--- app/src/main/java/com/example/myapplication/data/mapper/IntradayInfoMapper.kt
+++ app/src/main/java/com/example/myapplication/data/mapper/IntradayInfoMapper.kt
@@ -1,10 +1,15 @@
 package com.example.myapplication.data.mapper
+
+
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.myapplication.data.remote.dto.IntradayInfoDto
 import com.example.myapplication.domain.model.IntradayInfo
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
+@RequiresApi(Build.VERSION_CODES.O)
 fun IntradayInfoDto.toIntradayInfo(): IntradayInfo {
     val pattern = "yyyy-MM-dd HH:mm:ss"
     val formatter = DateTimeFormatter.ofPattern(pattern, Locale.getDefault())
