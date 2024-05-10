54579bbd08c6cb60a443266217943865603cfb96
Ahmet_Utku <94536064+AhmetUtk@users.noreply.github.com>
2024-05-10 23:44:31 +0300

    added stock api that new

--- app/src/main/java/com/example/myapplication/data/remote/StockApi.kt
+++ app/src/main/java/com/example/myapplication/data/remote/StockApi.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.data.remote
 
+import com.example.myapplication.data.remote.dto.CompanyInfoDto
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,18 +9,24 @@ import retrofit2.http.Query
 interface StockApi {
 
     @GET("query?function=LISTING_STATUS")
-
-
     suspend fun getListings(
+        @Query("apikey") apiKey: String = API_KEY
+    ): ResponseBody
 
-        @Query("apikey") apiKey:String = API_KEY
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
+    suspend fun getIntradayInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = API_KEY
+    ): ResponseBody
 
-    ):ResponseBody
+    @GET("query?function=OVERVIEW")
+    suspend fun getCompanyInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = API_KEY
+    ): CompanyInfoDto
 
-    companion object{
-        const val API_KEY = "VPLXZ9K5ML2BOW7W"
-        const val BASE_URL = "http://alphavantage.co"
+    companion object {
+        const val API_KEY = "G1USXWKX272RK4BP"
+        const val BASE_URL = "https://alphavantage.co"
     }
-
-
 }
