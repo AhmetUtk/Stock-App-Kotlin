334a43364fbe5adbe9c161596c5e7d94f56c6e33
Ahmet_Utku <94536064+AhmetUtk@users.noreply.github.com>
2024-05-10 23:44:10 +0300

    refactoring Resource

--- app/src/main/java/com/example/myapplication/util/Resource.kt
+++ app/src/main/java/com/example/myapplication/util/Resource.kt
@@ -2,6 +2,6 @@ package com.example.myapplication.util
 
 sealed class Resource <T>(val data: T? = null, val massage:String? = null){
     class Success<T>(data: T?):Resource<T>(data)
-    class Error<T>(massage: String, data: T? = null): Resource<T>(data,massage)
+    class Error<T>(message: String, data: T? = null): Resource<T>(data,message)
     class Loading<T>(val isLoading: Boolean = true): Resource<T>(null)
 }
