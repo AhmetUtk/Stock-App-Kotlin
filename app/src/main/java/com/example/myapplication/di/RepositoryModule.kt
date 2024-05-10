1ca92e696cfdcc583fe777de6eec8e00664cca9d
Ahmet_Utku <94536064+AhmetUtk@users.noreply.github.com>
2024-05-10 23:43:58 +0300

    added RepositoryModule

---
+++ app/src/main/java/com/example/myapplication/di/RepositoryModule.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication.di
+
+
+import com.example.myapplication.data.csv.CSVParser
+import com.example.myapplication.data.csv.CompanyListingsParser
+import com.example.myapplication.data.csv.IntradayInfoParser
+import com.example.myapplication.data.repostory.StockRepositoryImpl
+import com.example.myapplication.domain.model.CompanyListing
+import com.example.myapplication.domain.model.IntradayInfo
+import com.example.myapplication.domain.repostory.StockRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListingsParser(
+        companyListingsParser: CompanyListingsParser
+    ): CSVParser<CompanyListing>
+
+    @Binds
+    @Singleton
+    abstract fun bindIntradayInfoParser(
+        intradayInfoParser: IntradayInfoParser
+    ): CSVParser<IntradayInfo>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepositoryImpl: StockRepositoryImpl
+    ): StockRepository
+}
